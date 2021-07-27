320974f2c451d682a43e4b18829b4e26793aae79
Brymlee <brymlee1911@gmail.com>
2021-07-27 10:16:53 -0600

    Added Emperor social link

--- src/Main.hs
+++ src/Main.hs
@@ -81,6 +81,9 @@ kenji = Character (String "Kenji") $ String "Tomochika"
 fuuka :: Character
 fuuka = Character (String "Fuuka") $ String "Yamagishi"
 
+hidetoshi :: Character
+hidetoshi = Character (String "Hidetoshi") $ String "Odagiri"
+
 sees :: Group 
 sees = Group (String "SEES") [mitsuru, fuuka]
 
@@ -134,11 +137,23 @@ empress = SocialLink (Ranks (Rank1 (Automatic Nothing))
                             (Rank9 (Say (String "That makes me happy")))
                             (Rank10 (Automatic Nothing))) (String "Empress") $ Right mitsuru
 
+emperor :: SocialLink
+emperor = SocialLink (Ranks (Rank1 (Automatic Nothing))
+                            (Rank2 (Say (String "It's a waste of time")))
+                            (Rank3 (Say (String "Anything")))
+                            (Rank4 (Say (String "Anything -> Good work")))
+                            (Rank5 (Say (String "They can be pretty cruel")))
+                            (Rank6 (Say (String "Anything -> But I just got here")))
+                            (Rank7 (Say (String "He should suffer")))
+                            (Rank8 (Say (String "Anything -> It wasn't me")))
+                            (Rank9 (Say (String "Don't blame yourself")))
+                            (Rank10 (Automatic Nothing))) (String "Emperor") $ Right hidetoshi
+
 getValue :: State'
 getValue = do
   let characters = [mitsuru, kenji, fuuka]
   let groups = [sees]
-  let socialLinks = [fool, magician, priestess, empress]
+  let socialLinks = [fool, magician, priestess, empress, emperor]
   put (socialLinks, groups, characters)
   result <- get
   return result
