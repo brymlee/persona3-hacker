583c4a56695b6bb4fde001de84270991b770163b
Brymlee <brymlee1911@gmail.com>
2021-07-27 10:08:20 -0600

    Added Empress social link

--- src/Main.hs
+++ src/Main.hs
@@ -122,11 +122,23 @@ priestess = SocialLink (Ranks (Rank1 (Automatic Nothing))
                               (Rank9 (Say (String "Anthing -> Anything")))
                               (Rank10 (Automatic Nothing))) (String "Priestess") $ Right fuuka
 
+empress :: SocialLink
+empress = SocialLink (Ranks (Rank1 (Automatic Nothing))
+                            (Rank2 (Say (String "Anything")))
+                            (Rank3 (Say (String "Ask her to treat you -> Does that make you happy -> Anything -> Agree to forget about it")))
+                            (Rank4 (Say (String "Did something happen? -> It's the result of love -> Anything")))
+                            (Rank5 (Say (String "I'm glad you enjoyed it -> A motorcycle? -> Let's go for a ride")))
+                            (Rank6 (Say (String "Can I get a book for you -> That's news to me? -> You have to reconsider")))
+                            (Rank7 (Say (String "Let's do it")))
+                            (Rank8 (Say (String "Anything x 2 -> Say something to the guy -> Anything")))
+                            (Rank9 (Say (String "That makes me happy")))
+                            (Rank10 (Automatic Nothing))) (String "Empress") $ Right mitsuru
+
 getValue :: State'
 getValue = do
   let characters = [mitsuru, kenji, fuuka]
   let groups = [sees]
-  let socialLinks = [fool, magician, priestess]
+  let socialLinks = [fool, magician, priestess, empress]
   put (socialLinks, groups, characters)
   result <- get
   return result
